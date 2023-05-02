a24d89ac059fc345db1382b700a018d8ee745ffb
lucas <lucas.glasner@ceaza.cl>
2023-05-02 14:18:40 -0400

    starting repositoruy

---
+++ createS2S_climatology.py
@@ -0,0 +1,205 @@
+from glob import glob
+import xarray as xr
+import cartopy.crs as ccrs
+import cartopy.feature as cf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import scipy.signal as signal
+from scipy.interpolate import interp1d
+import os
+
+def filter_timeseries(ts, order, cutoff, btype='lowpass', fs=1, **kwargs):
+    """Given an array, this function apply a butterworth (high/low pass) 
+    filter of the given order and cutoff frequency.
+    For example:
+    If 'ts' is a timeseries of daily samples, filter_timeseries(ts,3,1/20)
+    will return the series without the 20 days or less variability using an
+    order 3 butterworth filter. 
+    In the same way, filter_timeseries(ts,3,1/20, btype='highpass') will
+    return the series with only the 20 days or less variability.
+
+    Args:
+        ts (array_like): timeseries or 1D array to filter
+        order (int): _description_
+        cutoff (array_like): Single float for lowpass or highpass filters, 
+        arraylike for bandpass filters.
+        btype (str, optional): The type of filter. Defaults to 'lowpass'.
+        fs (int): Sampling frequency. Defaults to 1
+        **kwargs are passed to scipy.signal.filtfilt
+
+    Returns:
+        output (array): Filtered array
+    """
+    mask = np.isnan(ts)
+    nans = np.ones(len(ts))*np.nan
+    if mask.sum()==len(ts):
+        return nans
+    else:
+        b, a = signal.butter(order, cutoff, btype=btype, fs=fs)
+        filt=signal.filtfilt(b, a, ts[~mask], **kwargs)
+        output=np.ones(len(ts))*np.nan
+        output[np.where(~mask)] = filt
+        return output
+    
+def filter_xarray(data, dim, order, cutoff, btype='lowpass', parallel=False, fs=1):
+    """Given a 3d DataArray, with time and spatial coordinates, this function apply
+    the 1D function filter_timeseries along the time dimension, filter the complete
+    xarray data.
+
+    Args:
+        data (XDataArray): data
+        dim (str): name of the time dimension
+        order (int): butterworth filter order
+        cutoff (array_like): if float, the cutoff frequency, if array must be the
+                            [min,max] frequencys for the bandpass filter.
+        btype (str, optional): {lowpass,highpass,bandpass}. Defaults to 'lowpass'.
+        parallel (bool, optional): If parallelize with dask. Defaults to False.
+        fs (int, optional): Sampling frequency. Defaults to 1.
+
+    Returns:
+        XDataArray: filtered data
+    """
+    if parallel:
+        dask='parallelized'
+    else:
+        dask='forbidden'
+    filt = xr.apply_ufunc(filter_timeseries, data, order, cutoff, btype, fs,
+                          input_core_dims=[[dim],[],[],[],[]],
+                          output_core_dims=[[dim]],
+                          exclude_dims=set((dim,)),
+                          keep_attrs=True,
+                          vectorize=True, dask=dask)
+    filt[dim] = data[dim]
+    return filt
+
+
+
+def climatology1d(data, times, period=365, nharmonics=3):
+    """
+    This function computes the annual cycle of a time series 
+    taking advantage of the fourier series decomposition and 
+    that seasonal changes are equal to the 365 time scale 
+    variability. 
+    
+    By defaults the function assumes the input data has a 
+    daily sampling, if not modify "period" argument properly. 
+
+    Args:
+        data (array): (n, ) dimensional array with time-ordered data
+        times (array):(n, ) dimensional array of time objects
+        period (int, optional): Period to remove. Defaults to 365.
+        nharmonics (int, optional): Number of harmonics related to 
+        the fundamental frequency to remove. Defaults to 3.
+
+    Returns:
+        array: (365, ) dimensional array with the climatological
+        value of each day.
+        
+    Reference: Statistical Methods in the Atmospheric Sciences
+              Daniel S. Wilks. Part II Univariate Statistics, 
+              Chapter 9.4: Frequency Domain - Harmonic Analysis
+              
+    Example 9.8: Transforming a Cosine Wave to Represent an Annual Cycle
+    Section 9.4.3: Estimation of the Amplitude and Phase of a Single Harmonic
+    Section 9.4.4: Higher Harmonics
+    Example 9.11: A more Complicated Annual Cycle
+    """
+    
+    # If data has nans return full nan vector
+    if np.isnan(data).sum()!=0:
+        return np.nan*np.ones(365)
+    # Transform data to pandas timeseries
+    data   = pd.Series(data, index = pd.to_datetime(times))
+    # Remove leap day of leap years and NaNs
+    data   = data[~((data.index.month==2)&(data.index.day==29))]
+    # Number of timestemps
+    n      = len(data) 
+    # Compute sample mean
+    clim  = np.mean(data)*np.ones(n)
+    for k in range(nharmonics):
+        # Target frequency
+        freq       = 2*np.pi*(k+1)*np.arange(1,n+1)/period
+        # Compute harmonic coefficient as a "naive" discrete
+        # fourier transform
+        Ak        = (2/n)*np.sum(data*np.cos(freq)) 
+        Bk        = (2/n)*np.sum(data*np.sin(freq)) 
+        # Add up harmonics to the annual cycle
+        clim      = clim+Ak*np.cos(freq)+Bk*np.sin(freq)
+    clim = pd.Series(clim, index=data.index).to_xarray().rename({'index':'time'})
+    clim = clim.to_dataset(name='_').convert_calendar('noleap')['_']
+    clim = clim.groupby("time.dayofyear").mean()
+    return clim.to_series()
+
+def climatology_xarray(data, dim, period=365, nharmonics=3, parallel=False, **kwargs):
+    """
+    Given a 3d DataArray, with time and spatial coordinates, this function apply
+    the 1D function climatology1d along the time dimension return the spatial
+    climatology.
+
+    Args:
+        data (_type_): _description_
+        dim (_type_): _description_
+        period (int, optional): _description_. Defaults to 365.
+        nharmonics (int, optional): _description_. Defaults to 3.
+        parallel (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+    if parallel:
+        dask='parallelized'
+    else:
+        dask='forbidden'
+    clim = xr.apply_ufunc(climatology1d, data, kwargs={'times':data.time,
+                                                       'period':period,
+                                                       'nharmonics':nharmonics},
+                          input_core_dims=[[dim],],
+                          output_core_dims=[[dim]],
+                          exclude_dims=set((dim,)),
+                          keep_attrs=True,
+                          vectorize=True, dask=dask,
+                          dask_gufunc_kwargs={'allow_rechunk':True},
+                          **kwargs)
+    clim = clim.rename({'time':'dayofyear'})
+    clim.coords['dayofyear'] = np.arange(1, len(clim.dayofyear)+1)
+    return clim
+
+print('loading reforecasts')
+reforecasts = sorted(glob("data/S2S/reforecasts/*[!.nc]"))
+DATA = []
+for p in reforecasts:
+    print(p)
+    if not os.path.isfile(p+'/merged_SST.nc'):
+
+        data = []
+        for r in glob(p+'/*SST.nc'):
+            dd = xr.open_dataset(r, chunks=dict(time=-1)).squeeze()
+            dd = dd.rename({'time':'leadtime'})
+            dd['leadtime'] = np.arange(len(dd['leadtime']))
+            dd['time'] = pd.to_datetime(r.split("/")[-1].replace("_SST.nc",""))
+            data.append(dd)
+        data = xr.concat(data,'time')
+        data.to_netcdf(p+'/merged_SST.nc')
+    else:
+        data = xr.open_dataset(p+'/merged_SST.nc').sortby('lat').sel(lat=slice(-60,15), lon=slice(360-150,360-69))
+        data = data.load()
+    DATA.append(data)
+print('Joining...')
+DATA = xr.concat(DATA,'time').sortby('time')
+DATA = DATA.sel(time=~((DATA.time.dt.month==2)&(DATA.time.dt.day==29)))
+DATA['lon'] = (DATA['lon']%180)-180
+DATA = DATA['sst'].drop_duplicates('time').resample({'time':'d'}).interpolate(kind='nearest')
+
+print(DATA)
+
+print('computing climatology')
+for i,lead in enumerate(DATA.leadtime):
+        clim = climatology_xarray(DATA.sel(leadtime=lead), 'time')
+        #anom = DATA.sel(leadtime=lead).dslm.convert_calendar('noleap').groupby('time.dayofyear')-clim
+        print('saving to netcdf: LEADTIME '+str(i))
+#for i,lead in enumerate(clim.leadtime):
+        clim.to_dataset(name='sst').to_netcdf('data/S2S/CLIMATOLOGY/SST_LEADTIME'+str(i)+'.nc',
+                                               encoding={'sst':{'dtype':'float32'}})
+       # anom.to_dataset(name='zos_anomaly').to_netcdf('data/S2S/reforecasts/i')    
