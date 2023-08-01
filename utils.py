f28d506fda29fa027346302f8af1edf17e4c4a95
lucas <lucas.glasner@ceaza.cl>
2023-08-01 11:55:04 -0400

    created ERA5 hovmoller and merged quikscat/ascat for chile peru coast

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import xarray as xr
+import scipy.signal as signal
 
 def haversine(p1,p2):
     """
@@ -123,4 +124,108 @@ def add_hovmoller_coordinatedata(tropical,coastnorth,coastsouth,mask,variables):
     coastsouth = coastsouth.swap_dims({'index':'distance'})
     coastsouth = coastsouth[variables]
     return tropical,coastnorth,coastsouth
+
+
+
+import scipy.signal as signal
+def filter_timeseries(ts, order, cutoff, btype='lowpass', fs=1, **kwargs):
+    """Given an array, this function apply a butterworth (high/low pass) 
+    filter of the given order and cutoff frequency.
+    For example:
+    If 'ts' is a timeseries of daily samples, filter_timeseries(ts,3,1/20)
+    will return the series without the 20 days or less variability using an
+    order 3 butterworth filter. 
+    In the same way, filter_timeseries(ts,3,1/20, btype='highpass') will
+    return the series with only the 20 days or less variability.
+
+    Args:
+        ts (array_like): timeseries or 1D array to filter
+        order (int): _description_
+        cutoff (array_like): Single float for lowpass or highpass filters, 
+        arraylike for bandpass filters.
+        btype (str, optional): The type of filter. Defaults to 'lowpass'.
+        fs (int): Sampling frequency. Defaults to 1
+        **kwargs are passed to scipy.signal.filtfilt
+
+    Returns:
+        output (array): Filtered array
+    """
+    mask = np.isnan(ts)
+    nans = np.ones(len(ts))*np.nan
+    if mask.sum()==len(ts):
+        return nans
+    else:
+        b, a = signal.butter(order, cutoff, btype=btype, fs=fs)
+        filt=signal.filtfilt(b, a, ts[~mask], padlen=30*max(len(a),len(b)), **kwargs)
+        output=np.ones(len(ts))*np.nan
+        output[np.where(~mask)] = filt
+        return output
+    
+def filter_xarray(data, dim, order, cutoff, btype='lowpass', parallel=False, fs=1):
+    """Given a 3d DataArray, with time and spatial coordinates, this function apply
+    the 1D function filter_timeseries along the time dimension, filter the complete
+    xarray data.
+
+    Args:
+        data (XDataArray): data
+        dim (str): name of the time dimension
+        order (int): butterworth filter order
+        cutoff (array_like): if float, the cutoff frequency, if array must be the
+                            [min,max] frequencys for the bandpass filter.
+        btype (str, optional): {lowpass,highpass,bandpass}. Defaults to 'lowpass'.
+        parallel (bool, optional): If parallelize with dask. Defaults to False.
+        fs (int, optional): Sampling frequency. Defaults to 1.
+
+    Returns:
+        XDataArray: filtered data
+    """
+    if parallel:
+        dask='parallelized'
+    else:
+        dask='forbidden'
+    filt = xr.apply_ufunc(filter_timeseries, data, order, cutoff, btype, fs,
+                          input_core_dims=[[dim],[],[],[],[]],
+                          output_core_dims=[[dim]],
+                          exclude_dims=set((dim,)),
+                          keep_attrs=True,
+                          vectorize=True, dask=dask)
+    filt[dim] = data[dim]
+    return filt
+
+
+def compute_climatology(data, dim='time', order=5, cutoff=1/90,
+                        kind='mean'):
+    """
+    This function computes a daily climatology with the following method:
+    1) Transform calendar to 'noleap' (same as remove 29 feb)
+    2) Compute the daily average
+    3) Repeat the daily (noisy) climatology for 30 years
+    4) Smooth with a lowpass filter
+    5) Compute again the daily average and return
+    
+    
+    Args:
+        data (xarray): dataset or datarray for climatology computing
+        dim (str, optional): Name of the time dimension. Defaults to 'time'.
+        order (int, optional): Butterworth filter order. Defaults to 5.
+        cutoff (float, optional): Cutoff frequency. Defaults to 1/80.
+        kind (str): 'mean' or 'std'
+
+    Returns:
+        _type_: _description_
+    """
+    clim = data.convert_calendar('noleap', dim=dim)
+    if kind=='mean':
+        clim = clim.groupby(dim+'.dayofyear').mean()
+    elif kind=='std':
+        clim = clim.groupby(dim+'.dayofyear').std()
+    else:
+        raise ValueError('"kind" parameter must be "mean" of "std"')
+    clim = xr.concat([clim]*30, 'dayofyear')
+    clim.coords[dim] = ('dayofyear',xr.date_range('2000','2030', calendar='noleap')[:-1])
+    clim = clim.swap_dims({'dayofyear':dim}).drop('dayofyear')
+    # clim = clim.rolling({dim:120}, center=True).mean()
+    clim = filter_xarray(clim, dim, order, cutoff)
+    clim = clim.groupby(dim+'.dayofyear').mean()
+    return clim
 
